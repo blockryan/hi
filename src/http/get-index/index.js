bdf59b8ece2a45c42e1895614178ce3092f56962
Bryan Black <38768803+blockryan@users.noreply.github.com>
2020-09-21 16:50:12 -0600

    Update index.js

--- src/http/get-index/index.js
+++ src/http/get-index/index.js
@@ -30,14 +30,15 @@ let body = `
 `
 
 exports.handler = async function http(req) {
-  return {
-    headers: {
-      'content-type': 'text/html; charset=utf8',
-      'cache-control': 'no-cache, no-store, must-revalidate, max-age=0, s-maxage=0'
-    },
-    statusCode: 200,
-    body
-  }
+  return req
+//   return {
+//     headers: {
+//       'content-type': 'text/html; charset=utf8',
+//       'cache-control': 'no-cache, no-store, must-revalidate, max-age=0, s-maxage=0'
+//     },
+//     statusCode: 200,
+//     body
+//   }
 }
 
 // Example responses
